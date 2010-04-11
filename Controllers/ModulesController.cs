ec243d6a271a447ac47f608d6efd10e9584b5c66
Michael James <mike@mjjames.co.uk>
2010-04-11 17:43:30 -0100

    new Calendar Entry DTO and What's Next Calendar Partial View integration

--- Controllers/ModulesController.cs
+++ Controllers/ModulesController.cs
@@ -1,15 +1,53 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using mjjames.MVC_MultiTenant_Controllers_and_Models.Models.DTO;
 
 namespace mjjames.MVC_MultiTenant_Controllers_and_Models.Controllers
 {
 	[HandleError]
 	public class ModulesController : Controller
 	{
+		/// <summary>
+		/// Gets the BibleGatewayVerseOfTheDay PartialView
+		/// </summary>
+		/// <param name="version">BibleGateway Bible Version</param>
+		/// <returns>PartialView</returns>
 		public ActionResult BibleGatewayVerseOfTheDay(BibleGateway.BibleTranslation version)
 		{
 			var verseoftheday = new BibleGateway.BibleGatewayWrapper().GetVerseOfTheDay(version);
 
 			return View("VerseOfTheDay", verseoftheday);
+		}
+
+		/// <summary>
+		/// Gets the WhatsOnNext PartialView using Google Calendars
+		/// </summary>
+		/// <param name="feedURL">Google Calendar Feed URL</param>
+		/// <returns>PartialView</returns>
+		public ActionResult WhatsOnNext(string feedURL)
+		{
+			//get our  next event entry
+			var eventEntry = new GoogleCalendarWrapper.GoogleCalendar(feedURL).GetNextEvent();
+			//convert the eventEntry into a CalendarEntry
+			var whatsonNext = new CalendaryEntryDTO();
+
+			if(eventEntry != null)
+			{
+				whatsonNext = new CalendaryEntryDTO
+				              	{
+				              		Description = eventEntry.Summary.Text,
+				              		Location = eventEntry.Locations[0] != null ? eventEntry.Locations[0].ValueString : "",
+				              		Title = eventEntry.Title.Text,
+				              		End = eventEntry.Times[0] != null ? eventEntry.Times[0].EndTime : new DateTime(),
+				              		Start = eventEntry.Times[0] != null ? eventEntry.Times[0].StartTime : new DateTime()
+				              	};
+			}
+			else
+			{
+				whatsonNext.Title = "Event Information Unavailable at this time";
+			}
+
+			return View("WhatsOnNext", whatsonNext);
 		}
 	}
 }
---
+++ Models/DTO/CalendaryEntryDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace mjjames.MVC_MultiTenant_Controllers_and_Models.Models.DTO
+{
+	public class CalendaryEntryDTO
+	{
+		public string Title{ get; set;}
+		public string Location{ get; set;}
+		public string Description { get;set;}
+		public DateTime Start { get; set;}
+		public DateTime End { get; set;}
+
+	}
+}
