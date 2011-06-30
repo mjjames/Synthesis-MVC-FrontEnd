249844bbf7c0f8a66e8463a6455cf68a028f37c7
Michael James <mike@mjjames.co.uk>
2011-06-30 19:10:06 -0100

    missing factory for external modules

---
+++ Factories/ExternalModulesFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using mjjames.GoogleCalendarWrapper;
+
+namespace mjjames.MVC_MultiTenant_Controllers_and_Models.Factories
+{
+	/// <summary>
+	/// Provides factory methods for loading external modules
+	/// </summary>
+	public class ExternalModulesFactory
+	{
+		/// <summary>
+		/// Returns a Google Calendar Provider
+		/// </summary>
+		/// <param name="calendarUrl">URL to the calendar feed</param>
+		/// <returns></returns>
+		public static IGoogleCalendar GetCalendarInstance(string calendarUrl)
+		{
+#if DEBUG
+			//if in debug mode always return the test wrapper which has sample data
+			return new TestGoogleCalendar();
+#endif
+
+			//return the full Google Calendar Wrappeer
+			return new GoogleCalendar(calendarUrl);
+		}
+	}
+}
