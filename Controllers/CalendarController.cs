f81ca78abccbba307143511d97233be42c4dfd1a
Michael James <mike@mjjames.co.uk>
2011-07-10 16:55:59 -0100

    - having no future events would cause an error, it now uses the last event, if there are no events a message is shown - repeat events also didnt work, they now are generated from the original calendar entries, these entries have one time per repeat - changed the id of the calendar event to be the final url segment of the calendar event id but also the datetime of it, in ticks, due to repeated events

--- Controllers/CalendarController.cs
+++ Controllers/CalendarController.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
-using System.Globalization;
-using mjjames.MVC_MultiTenant_Controllers_and_Models.Models;
-using mjjames.MVC_MultiTenant_Controllers_and_Models.Repositories;
-using mjjames.MVC_MultiTenant_Controllers_and_Models.Models.DTO;
 using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Web.Mvc;
 using mjjames.MVC_MultiTenant_Controllers_and_Models.Factories;
+using mjjames.MVC_MultiTenant_Controllers_and_Models.Models;
+using mjjames.MVC_MultiTenant_Controllers_and_Models.Models.DTO;
+using mjjames.MVC_MultiTenant_Controllers_and_Models.Repositories;
 
 namespace mjjames.MVC_MultiTenant_Controllers_and_Models.Controllers
 {
@@ -20,28 +19,39 @@ namespace mjjames.MVC_MultiTenant_Controllers_and_Models.Controllers
 		//    return Calendar(startDate, String.Empty);
 		//}
 
-		public ActionResult Calendar(string startDate, string eventID){
+		public ActionResult Calendar(string startDate, string eventID)
+		{
 			var date = new DateTime();
-			if (!DateTime.TryParseExact(startDate, "yyyy-MM", new CultureInfo("en-GB") , DateTimeStyles.None,  out date))
+			if (!DateTime.TryParseExact(startDate, "yyyy-MM", new CultureInfo("en-GB"), DateTimeStyles.None, out date))
 			{
 				date = DateTime.Now;
 			}
 			var events = GetCalendarEvents(ConfigurationManager.AppSettings["GoogleCalendarFeedURL"], date, date.AddMonths(1));
-			var	entry = String.IsNullOrWhiteSpace(eventID) ?  events.FirstOrDefault(e => e.Start > DateTime.Today) : events.FirstOrDefault(e => e.Identifier.Equals(eventID));
-			
+			var entry = String.IsNullOrWhiteSpace(eventID) ? events.FirstOrDefault(e => e.Start > DateTime.Today) : events.FirstOrDefault(e => e.Identifier.Equals(eventID));
+
 			//if we don't have a valid entry and we have been provided an event ID return a 404
-			if(entry == null && !String.IsNullOrEmpty(eventID)){
+			if (entry == null && !String.IsNullOrEmpty(eventID))
+			{
 				return new HttpNotFoundResult();
 			}
-		
-			var model = new CalendarModel(){
+			if (entry == null)
+			{
+				entry = events.LastOrDefault();
+			}
+			if (entry == null)
+			{
+				entry = new CalendarEntryDTO { Start = DateTime.Today, Title = "No Further Events This Monrth" };
+			}
+
+			var model = new CalendarModel()
+			{
 				Date = date,
 				MainNavigation = _navs.GetMainNavigation().ToList(),
 				FooterNavigation = _navs.GetFooterNavigation().ToList(),
 				Events = events,
-				Event = entry,
+				Event = entry ?? new CalendarEntryDTO(),
 				IsDetailsPage = String.IsNullOrEmpty(eventID),
-				PageTitle = String.Format("{0} - {1} - {2}", 
+				PageTitle = String.Format("{0} - {1} - {2}",
 					String.IsNullOrEmpty(eventID) ? ConfigurationManager.AppSettings["CalendarPageTitle"] : entry.Title,
 					date.ToString("MMMM yyyy"),
 					ConfigurationManager.AppSettings["SiteName"]
@@ -72,16 +82,31 @@ namespace mjjames.MVC_MultiTenant_Controllers_and_Models.Controllers
 		{
 			//get a custom range view of events
 			var eventEntries = ExternalModulesFactory.GetCalendarInstance(feedURL)
-				.GetEvents(startDate, endDate).Select(eventEntry => new CalendarEntryDTO
+				.GetEvents(startDate, endDate);
+
+			//we cant just spit out event entries
+			//as repeat events simply have multiple times
+			//instead loop through each entry and then each time adding the events
+			var entryDTOs = new List<CalendarEntryDTO>();
+			foreach (var entry in eventEntries)
+			{
+				foreach (var time in entry.Times)
 				{
-					Identifier = eventEntry.Id.AbsoluteUri,
-					Description = eventEntry.Content != null ? eventEntry.Content.Content : "",
-					Location = eventEntry.Locations.Count > 0 ? eventEntry.Locations[0].ValueString : "",
-					Title = eventEntry.Title.Text,
-					End = eventEntry.Times.Count > 0 ? eventEntry.Times[0].EndTime : new DateTime(),
-					Start = eventEntry.Times.Count > 0 ? eventEntry.Times[0].StartTime : new DateTime()
-				}).OrderBy(e => e.Start).ToList();
-			return eventEntries;
+					var idUrl = new Uri(entry.Id.AbsoluteUri);
+					entryDTOs.Add(new CalendarEntryDTO
+					{
+						Identifier = string.Format("{0}-{1}", idUrl.Segments.Last(), time.StartTime.Ticks),
+						Description = entry.Content != null ? entry.Content.Content : "",
+						Location = entry.Locations.Count > 0 ? entry.Locations[0].ValueString : "",
+						Title = entry.Title.Text,
+						Start = time.StartTime,
+						End = time.EndTime
+					});
+				}
+			}
+
+			//then on return order by start date
+			return entryDTOs.OrderBy(e => e.Start).ToList();
 		}
 	}
 }
