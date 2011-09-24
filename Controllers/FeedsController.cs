e89a6b1cc1ca5b3cc26e8129189c12e1128be6c3
Michael James <mike@mjjames.co.uk>
2011-09-24 21:25:02 -0100

    added the ability to have different types of podcast rss feeds

--- Controllers/FeedsController.cs
+++ Controllers/FeedsController.cs
@@ -1,44 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
-using System.Text;
 using System.Web.Mvc;
-using mjjames.MVC_MultiTenant_Controllers_and_Models.Repositories;
+using mjjames.MVC_MultiTenant_Controllers_and_Models.Models;
 using mjjames.MVC_MultiTenant_Controllers_and_Models.Models.DTO;
 using mjjames.MVC_MultiTenant_Controllers_and_Models.Models.Feeds;
-using System.Configuration;
+using mjjames.MVC_MultiTenant_Controllers_and_Models.Repositories;
 
 namespace mjjames.MVC_MultiTenant_Controllers_and_Models.Controllers
 {
-	public class FeedsController : Controller
-	{
-		private readonly PodcastRepository _podcasts = new PodcastRepository();
+    public class FeedsController : Controller
+    {
+        private readonly PodcastRepository _podcasts = new PodcastRepository();
 
-		public ActionResult Podcasts(string type)
-		{
-			this.HttpContext.Response.AddHeader("Content-Type", "text/xml");
-			var data = _podcasts.FindAllActive().ToList();
+        public ActionResult Podcasts(string feedType)
+        {
+            return PodcastsByType(PodcastType.Podcast, feedType);
+        }
 
-			var buildDate = data.Max(p => p.Published);
 
-			var feed = new PodcastFeed
-			{
-				Title = ConfigurationManager.AppSettings["SiteName"] + " - Podcasts Feed",
-				Link = HttpContext.Request.Url.ToString(),
-				Description = ConfigurationManager.AppSettings["PodcastDescription"],
-				BuildDate = buildDate.HasValue ? buildDate.Value : DateTime.Now,
-				PubDate = DateTime.Now,
-				FeedItems = data.Select(p => new PodcastDTO()
-				{
-					Title = p.Title,
-					Description = p.Description,
-					FileName = p.Filename.Replace(" ", "+"),
-					Published = p.Published
-				}).AsEnumerable(),
 
-			};
+        public ActionResult PodcastsByType(PodcastType podcastType, string feedType)
+        {
+            this.HttpContext.Response.AddHeader("Content-Type", "text/xml");
+            var data = _podcasts.GetPodcastsByType(podcastType).ToList();
 
-			return View(type.ToUpper(), feed);
-		}
-	}
+            var buildDate = data.Max(p => p.Published);
+
+            var feed = new PodcastFeed
+            {
+                Title = String.Format("{0} - {1}s {2} Feed", ConfigurationManager.AppSettings["SiteName"], podcastType, feedType.ToUpper()),
+                Link = HttpContext.Request.Url.ToString(),
+                Description = ConfigurationManager.AppSettings[podcastType + "-Description"],
+                BuildDate = buildDate.HasValue ? buildDate.Value : DateTime.Now,
+                PubDate = DateTime.Now,
+                FeedItems = data.Select(p => new PodcastDTO()
+                {
+                    Title = p.Title,
+                    Description = p.Description,
+                    FileName = p.Filename.Replace(" ", "+"),
+                    Published = p.Published
+                }).AsEnumerable(),
+
+            };
+
+            return View(feedType.ToUpper(), feed);
+        }
+    }
 }
