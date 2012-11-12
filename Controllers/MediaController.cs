43ed167ce743bc9f4852613ff08b88e8d81b2af1
Michael James <michael.james@mksoftwaresolutions.co.uk>
2012-11-12 23:10:29 +0000

    bannrs now use the number value and its optional new viewName to allow custom rendering of media banners are randomised before being trimmed

--- Controllers/MediaController.cs
+++ Controllers/MediaController.cs
@@ -15,16 +15,18 @@ namespace mjjames.MVC_MultiTenant_Controllers_and_Models.Controllers
 		private readonly MediaRepository _mediaRepository = new MediaRepository();
         
         [ChildActionOnly]
-		public ActionResult Banners(MediaType mediaType, int number)
+		public ActionResult Banners(MediaType mediaType, int number = 4, string viewName = "Banners")
 		{
-			var banners = _mediaRepository.GetByLookupID(mediaType.ToString()).Select( m => new MediaDTO{
+			var banners = _mediaRepository.GetByLookupID(mediaType.ToString())
+                .Select( m => new MediaDTO{
 				Description = m.description,
 				FileName = m.filename,
 				Link = m.link,
 				Title  = m.title,
-			}).ToList();
+                });
+            var orderedBanners = banners.AsEnumerable().OrderBy(s => Guid.NewGuid());
 
-			return View("Banners", banners);
+            return View(viewName, orderedBanners.Take(number));
 		}
 	}
 }
